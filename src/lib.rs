038db8df42d72c68a04db265b30953aafda92e5e
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-03-29 01:11:30 +0900

    add new method

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,27 @@ pub struct ToyVec<T> {
     elements: Box<[T]>, // T型の要素を格納する領域。各要素はヒープ領域に置かれる
     len: usize,         // ベクタの長さ（現在の要素数）
 }
+
+impl<T: Default> ToyVec<T> {
+    // new はキャパシティ（容量）が0のToyVecを作る
+    pub fn new() -> Self {
+       Self::with_capacity(0) 
+    }
+
+    // with_capacityは指定されたキャパシティを持つToyVecを作る
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            elements: Self::allocate_in_heap(capacity),
+            len: 0,
+        }
+    }
+
+    // T型の値がsize個格納できるBox<[T]>を返す
+    pub fn allocate_in_heap(size: usize) -> Box<[T]> {
+        std::iter::repeat_with(Default::default())
+            .take(size) // T型のデフォルト値をsize個作り
+            .collect::<Vec<_>>() // Vec<T>に収集してから
+            .into_boxed_slice() // Box<[T]>に変換する
+    }
+
+}
